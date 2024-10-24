414bce828d3a3aafd83af139ba7bff1103e57c89
Jeong yong hwan <baechoo0905@naver.com>
2024-10-24 14:04:49 +0900

    style: 사이드 바의 header와 메인 콘텐츠의 topbar 높이 통일

--- src/components/MainContent/TopBar.js
+++ src/components/MainContent/TopBar.js
@@ -4,25 +4,30 @@ import IconButton from '../common/IconButton';
 
 const TopBar = ({ isSidebarOpen, toggleSidebar }) => {
     return (
-        <div className="h-12 border-b border-gray-200 flex items-center justify-between px-4 bg-white">
-            <div className="flex items-center space-x-2">
-                {!isSidebarOpen && (
-                    <IconButton onClick={toggleSidebar} className="mr-2">
-                        <ChevronsRight className="h-5 w-5 text-gray-500" />
-                    </IconButton>
-                )}
-                <IconButton>
-                    <Share2 className="h-4 w-4 text-gray-500" />
-                </IconButton>
-                <IconButton>
-                    <Star className="h-4 w-4 text-gray-500" />
-                </IconButton>
-                <IconButton>
-                    <Clock className="h-4 w-4 text-gray-500" />
-                </IconButton>
-            </div>
+        <div className="h-11 border-gray-200 flex items-center justify-between px-4 py-4 bg-white">
             <div className="flex items-center">
-                <IconButton>
+                {!isSidebarOpen && (
+                    <div className="mr-3">
+                        <IconButton onClick={toggleSidebar}>
+                            <ChevronsRight className="h-4 w-4 text-gray-500" />
+                        </IconButton>
+                    </div>
+                )}
+                <div className="flex items-center space-x-1">
+                    <IconButton tooltipText="공유">
+                        <Share2 className="h-4 w-4 text-gray-500" />
+                    </IconButton>
+                    <IconButton tooltipText="즐겨찾기">
+                        <Star className="h-4 w-4 text-gray-500" />
+                    </IconButton>
+                    <IconButton tooltipText="기록">
+                        <Clock className="h-4 w-4 text-gray-500" />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="flex items-center">
+                <IconButton tooltipText="설정">
                     <Settings className="h-4 w-4 text-gray-500" />
                 </IconButton>
             </div>
