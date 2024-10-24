32e697125d4aa0e4cfef38f6df4cbb83dee48b38
Jeong yong hwan <baechoo0905@naver.com>
2024-10-24 15:33:10 +0900

    style: 사이드바 닫힘 버튼 위치 수정

--- src/components/Sidebar/SidebarHeader.js
+++ src/components/Sidebar/SidebarHeader.js
@@ -7,28 +7,27 @@ const SidebarHeader = ({ toggleSidebar, isHoveringTitle, setIsHoveringTitle }) =
         <div className="h-11 flex items-center justify-between px-3 border-b border-gray-200">
             {/* Left side with hover area */}
             <div className="flex-1 flex items-center justify-between">
-                <div
-                    className="flex items-center space-x-2 cursor-pointer group"
-                    onMouseEnter={() => setIsHoveringTitle(true)}
-                    onMouseLeave={() => setIsHoveringTitle(false)}
+                <div className="flex items-center space-x-2">
+                    <span className="text-sm text-gray-700">열린 작업 No...</span>
+                    <ChevronDown className="h-4 w-4 text-gray-500"/>
+                </div>
+                <div className="flex"
+                     onMouseEnter={() => setIsHoveringTitle(true)}
+                     onMouseLeave={() => setIsHoveringTitle(false)}
                 >
-                    <div className="flex items-center space-x-2">
-                        <span className="text-sm text-gray-700">열린 작업 No...</span>
-                        <ChevronDown className="h-4 w-4 text-gray-500" />
-                    </div>
-
                     {/* Close button with hover effect */}
-                    <div className={`transition-opacity duration-200 ${isHoveringTitle ? 'opacity-100' : 'opacity-0'}`}>
+                    <div
+                        className={`transition-opacity duration-200 ${isHoveringTitle ? 'opacity-100' : 'opacity-0'}`}>
                         <IconButton onClick={toggleSidebar} className="ml-2">
-                            <ChevronsLeft className="h-4 w-4 text-gray-500" />
+                            <ChevronsLeft className="h-4 w-4 text-gray-500"/>
                         </IconButton>
                     </div>
-                </div>
 
-                {/* Right side with Plus button */}
-                <IconButton>
-                    <SquarePen className="h-4 w-4 text-gray-500 hover:text-gray-800" />
-                </IconButton>
+                    {/* Right side with Plus button */}
+                    <IconButton>
+                        <SquarePen className="h-4 w-4 text-gray-500 hover:text-gray-800"/>
+                    </IconButton>
+                </div>
             </div>
         </div>
     );
